3f25f1af0d953a7d2e01e42d9d67668e06ae10ae
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-09 23:53:02 +0530

    Add Header and loginPage into aBroweRouter tag

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from "./Components_UI/Header/Header";
 import { Home } from "./Pages/Home";
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { LoginPage } from "./Pages/Login/LoginPage";
 function App() {
+
+  
+
   return(
     <>
-      <Header/>
-      <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+      
+
     </>
   );
 }
