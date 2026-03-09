95ec23f54ac231cd914adfec8a2ef883208b7ea9
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-09 23:27:49 +0530

    Add loginPage.jsx

---
+++ E-commerce/src/Pages/Login/LoginPage.jsx
@@ -0,0 +1,13 @@
+
+
+export const LoginPage = () =>{
+    return(
+        <>
+            <div>
+                <input type="text" /> <br />
+                <input type="passwor" name="" id="" /><br />
+                <button>Sumbit</button>
+            </div>
+        </>
+    );
+}
