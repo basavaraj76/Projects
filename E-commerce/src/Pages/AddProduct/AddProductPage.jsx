20c8d5b5e744737a7722ce79087e5968e0f21400
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-10 23:09:39 +0530

    Add product page

---
+++ E-commerce/src/Pages/AddProduct/AddProductPage.jsx
@@ -0,0 +1,10 @@
+
+
+export const AddProductPage = () =>{
+    return(
+        <>
+            Product Name : <input type="text" /><br />
+            Product Price : <input type="Number" name="" id=""  />
+        </>
+    )
+}
