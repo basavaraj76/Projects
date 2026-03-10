464c5d2fdc31604daca4f5506b6d1e350d31571c
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-11 00:06:00 +0530

    Change BrowserRouter tag to createBrowerRouter function

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,19 +1,39 @@
 import { Header } from "./Components_UI/Header/Header";
 import { Home } from "./Pages/Home";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route, createBrowserRouter, RouterProvider} from "react-router-dom"
 import { LoginPage } from "./Pages/Login/LoginPage";
+import { AddProductPage } from "./Pages/AddProduct/AddProductPage";
+
 function App() {
 
+  const router = createBrowserRouter([
+      {
+        path:"/",
+        element:<Header/>
+      },
+      {
+        path:"/login",
+        element : <LoginPage/>
+      },
+      {
+        path:"/addproduct",
+        element : <AddProductPage></AddProductPage>
+      }
+    ])
   
 
   return(
+    
     <>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/addproduct" element={<AddProductPage/>} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    <RouterProvider router={router} />
       
 
     </>
