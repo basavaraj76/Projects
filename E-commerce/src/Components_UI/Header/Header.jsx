4038c8b141fe3669eed09f2b1c42f472ae6a93c5
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-03-15 01:54:46 +0530

    Change the addproduct endpoint path

--- E-commerce/src/Components_UI/Header/Header.jsx
+++ E-commerce/src/Components_UI/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "/src/Components_UI/Header/Header.module.css"
 import { Search } from "../SearchBar/Search";
 import { Login } from "../Login/Login";
 import { Cart } from "../Cart/Cart";
-import { AddProduct } from "../AddProductButton.jsx/addProduct";
+import { AddProduct } from "../AddProductButton/AddProduct";
 export const Header = () =>{
     return(
         <>
